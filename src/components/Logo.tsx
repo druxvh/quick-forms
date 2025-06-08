e3717656c0733de1974fa9860217c02373684c9c
dm <druavhm@gmail.com>
2025-06-09 02:46:42 +0530

    feat(ui): add `<Logo>` component

---
+++ src/components/Logo.tsx
@@ -0,0 +1,9 @@
+import Link from "next/link"
+
+function Logo() {
+    return (
+        <Link href={'/'} className="text-lg font-mono font-semibold tracking-wider">quick-forms</Link>
+    )
+}
+
+export default Logo
