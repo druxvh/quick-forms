71d49a1b59af1bf817192ef07bc687a2fd5a4074
dm <druavhm@gmail.com>
2025-06-09 02:50:50 +0530

    refactor: update globals css `:root` & style dashboard layout

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,9 +1,18 @@
+import Logo from "@/components/Logo"
+import ThemeSwitcher from "@/components/ThemeSwitcher"
+import { UserButton } from "@clerk/nextjs"
 import { ReactNode } from "react"
 
 export default function Layout({ children }: { children: ReactNode }) {
     return (
-        <div className="flex outline flex-col min-h-screen min-w-full bg-background max-h-screen">
-            <nav></nav>
+        <div className="flex flex-col min-h-screen min-w-full bg-background max-h-screen">
+            <nav className="h-16 px-4 py-2 outline flex justify-between items-center">
+                <Logo />
+                <div className="flex gap-5">
+                    <ThemeSwitcher />
+                    <UserButton />
+                </div>
+            </nav>
             <main className="flex w-full flex-grow">{children}</main>
         </div>
     )
