d4ae4c42607c2596fcb4d2eb8c7040533b1fd06c
dm <druavhm@gmail.com>
2025-06-22 03:07:20 +0530

    feat(actions): add `createForm` server action

--- actions/form.ts
+++ actions/form.ts
@@ -1,6 +1,7 @@
 "use server"
 
 import prisma from "@/lib/prisma"
+import { formSchema, formSchemaType } from "@/schemas/form"
 import { currentUser } from "@clerk/nextjs/server"
 
 class UserNotFoundErr extends Error {
@@ -54,4 +55,25 @@ export async function getFormStats(): Promise<{
             bounceRate: 0
         }
     }
+}
+
+export async function createForm(data: formSchemaType) {
+    const validation = formSchema.safeParse(data)
+    if (!validation.success) throw new Error("form not valid")
+
+    const user = await currentUser()
+    if (!user) throw new UserNotFoundErr()
+
+    const { name, description } = data
+
+    const form = await prisma.form.create({
+        data: {
+            userId: user.id,
+            name,
+            description
+        }
+    })
+    if (!form) throw new Error("Something went wrong")
+
+    return form.id
 }
