b91eacec4a49603ceb9fbbc2f2f614059d58e770
dm <druavhm@gmail.com>
2025-08-16 00:41:29 +0530

    feat: show element skeleton on overlay while dragging

--- src/components/DragOverlayWrapper.tsx
+++ src/components/DragOverlayWrapper.tsx
@@ -38,6 +38,15 @@ export default function DragOverlayWrapper() {
         const elementId = draggedItem.data?.current?.elementId
         const element = elements.find((el) => el.id === elementId)
         if (!element) node = <div>Element Not Found</div>
+        else {
+            const DesignerElementComponent = FormElements[element.type].designerComponent
+
+            node = (
+                <div className='flex bg-accent border rounded-md h-[120px] w-full p-4 opacity-50 pointer-events-none'>
+                    <DesignerElementComponent elementInstance={element} />
+                </div>
+            )
+        }
     }
 
     return (
