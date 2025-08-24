9c5a8d8d2ff4f6d3d44adceea872f2b0184a7448
dm <druavhm@gmail.com>
2025-08-25 02:40:22 +0530

    chore: add nextjs-toploader

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import { ClerkProvider } from "@clerk/nextjs";
 import { ThemeProvider } from "@/components/theme-provider/theme-provider";
 import { Toaster } from "@/components/ui/sonner";
 import DesignerContextProvider from "@/context/DesignerContext";
+import NextTopLoader from "nextjs-toploader"
 
 export const metadata: Metadata = {
   title: "Quick Forms - Make Forms in Minutes!",
@@ -19,6 +20,7 @@ export default function RootLayout({
     <ClerkProvider publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY}>
       <html lang="en" suppressHydrationWarning>
         <body className="antialiased">
+          <NextTopLoader color="#EF6461" showSpinner={false} />
           <DesignerContextProvider>
             <ThemeProvider
               attribute="class"
