48dfc7f4f973f86a635fc06a32e75cdbfd208c3d
dm <druavhm@gmail.com>
2025-11-22 03:05:05 +0530

    chore: update Prisma dependencies and configuration for v7.0.0
    - Add prisma.config.ts for configuration management
    - Update schema.prisma with new enums for Role and Goal, and modify User model
    - Adjust prisma.ts to use the new PrismaClient import and configuration
    - Refactor middleware to include onboarding route in protected routes

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,13 +1,16 @@
-import { PrismaClient } from '@/generated/prisma'
+import { PrismaClient } from '@/generated/prisma/client'
 import { withAccelerate } from '@prisma/extension-accelerate'
 
 const globalForPrisma = global as unknown as {
     prisma: PrismaClient
 }
 
-const prisma = globalForPrisma.prisma || new PrismaClient().$extends(withAccelerate())
+const prisma = globalForPrisma.prisma ?? new PrismaClient({
+    accelerateUrl: process.env.DATABASE_URL!
+}).$extends(withAccelerate())
 
-if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
+if (process.env.NODE_ENV !== 'production') {
+    globalForPrisma.prisma = prisma
+}
 
 export default prisma
-
--- src/middleware.ts
+++ src/middleware.ts
@@ -1,17 +1,16 @@
 import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
 
-// const isPublicRoute = createRouteMatcher(['/sign-in(.*)'])
-
-// export default clerkMiddleware(async (auth, req) => {
-//   if (!isPublicRoute(req)) {
-//     await auth.protect()
-//   }
-// })
-
-const isProtectedRoute = createRouteMatcher(['/dashboard(.*)', '/forms(.*)', '/builder(.*)'])
+const isProtectedRoute = createRouteMatcher([
+  '/dashboard(.*)',
+  '/forms(.*)',
+  '/builder(.*)',
+  '/onboarding'
+])
 
 export default clerkMiddleware(async (auth, req) => {
   if (isProtectedRoute(req)) await auth.protect()
+
+  return;
 })
 
 export const config = {
