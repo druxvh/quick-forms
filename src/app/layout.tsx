05df45229d46d88662e51393152469eaf88dec92
dm <druavhm@gmail.com>
2025-12-06 01:23:23 +0530

    feat: update metadata and add new icons for improved SEO and branding

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,9 +7,53 @@ import NextTopLoader from 'nextjs-toploader';
 import { Analytics } from '@vercel/analytics/next';
 
 export const metadata: Metadata = {
-    title: 'QForms - Make Forms in Minutes!',
+    title: {
+        default: 'QForms | Drag & Drop Form Builder',
+        template: '%s | QForms',
+    },
     description:
-        'QForms is an online form making software being an alternative to Google Forms with better UI, Drag-n-Drop customizable form components!',
+        'Build beautiful, powerful forms in seconds. QForms is a fully drag-and-drop form builder with support for 10+ field types, designed for easy form building, and seamless data collection.',
+
+    keywords: [
+        'form builder',
+        'drag and drop forms',
+        'online form creator',
+        'data collection tool',
+        'Next.js form app',
+        'QForms',
+    ],
+
+    icons: {
+        icon: '/favicon.ico',
+        apple: '/apple-icon.png',
+    },
+
+    openGraph: {
+        title: 'QForms: The Intuitive Drag & Drop Form Builder',
+        description:
+            'Create advanced, 10+ field forms with zero code. Simply drag, drop, and deploy.',
+        url: 'https://www.tryqforms.vercel.app',
+        siteName: 'QForms',
+        images: [
+            {
+                url: '/og-image.png',
+                width: 1200,
+                height: 630,
+                alt: 'QForms Drag and Drop Form Builder Interface',
+            },
+        ],
+        locale: 'en_US',
+        type: 'website',
+    },
+
+    twitter: {
+        card: 'summary_large_image',
+        title: 'QForms | Fast, Powerful D&D Form Builder',
+        description:
+            '10+ fields, no code, just drag and drop. Start building better forms today.',
+        creator: '@druxvh',
+        images: ['/og-image.png'],
+    },
 };
 
 export default function RootLayout({
