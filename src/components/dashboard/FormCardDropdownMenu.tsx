3d94fb9e4f0bead4dd40d6992deba7e97454df21
dm <druavhm@gmail.com>
2025-12-07 21:27:14 +0530

    feat: add delete comfirmation alert dialog for form card

--- src/components/dashboard/FormCardDropdownMenu.tsx
+++ src/components/dashboard/FormCardDropdownMenu.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { MoreVerticalIcon } from 'lucide-react';
+import { MoreVerticalIcon, TriangleAlertIcon } from 'lucide-react';
 import { Button } from '@/components/ui/button';
 import {
     DropdownMenu,
@@ -12,12 +12,24 @@ import {
 } from '@/components/ui/dropdown-menu';
 import { deleteFormByIdAction } from '@/actions/form';
 import { useRouter } from 'next/navigation';
-import { useTransition } from 'react';
+import { useState, useTransition } from 'react';
 import { toast } from 'sonner';
+import {
+    AlertDialog,
+    AlertDialogAction,
+    AlertDialogCancel,
+    AlertDialogContent,
+    AlertDialogDescription,
+    AlertDialogFooter,
+    AlertDialogHeader,
+    AlertDialogTitle,
+    AlertDialogTrigger,
+} from '../ui/alert-dialog';
 
 export function FormCardDropdownMenu({ formId }: { formId: string }) {
     const router = useRouter();
     const [isPending, startTransition] = useTransition();
+    const [isDialogOpen, setIsDialogOpen] = useState(false);
 
     const handleDelete = () => {
         startTransition(async () => {
@@ -44,40 +56,78 @@ export function FormCardDropdownMenu({ formId }: { formId: string }) {
                         '--normal-border': 'var(--destructive)',
                     } as React.CSSProperties,
                 });
+            } finally {
+                setIsDialogOpen(false);
             }
         });
     };
 
     return (
-        <DropdownMenu modal={false}>
-            <DropdownMenuTrigger asChild>
-                <Button
-                    variant="outline"
-                    aria-label="Open menu"
-                    size="icon"
-                    className="cursor-pointer"
-                >
-                    <MoreVerticalIcon />
-                </Button>
-            </DropdownMenuTrigger>
-            <DropdownMenuContent className="w-40" align="end">
-                <DropdownMenuLabel hidden>Form Actions</DropdownMenuLabel>
-                <DropdownMenuGroup>
-                    <DropdownMenuItem
+        <AlertDialog open={isDialogOpen} onOpenChange={setIsDialogOpen}>
+            <DropdownMenu modal={false}>
+                <DropdownMenuTrigger asChild>
+                    <Button
+                        variant="outline"
+                        aria-label="Open menu"
+                        size="icon"
                         className="cursor-pointer"
-                        onSelect={() => console.log('analytics')}
                     >
-                        Analytics
-                    </DropdownMenuItem>
-                    <DropdownMenuItem
+                        <MoreVerticalIcon />
+                    </Button>
+                </DropdownMenuTrigger>
+                <DropdownMenuContent className="w-40" align="end">
+                    <DropdownMenuLabel hidden>Form Actions</DropdownMenuLabel>
+                    <DropdownMenuGroup>
+                        {/* <DropdownMenuItem
+                            className="cursor-pointer"
+                            onSelect={() => console.log('analytics')}
+                        >
+                            Analytics
+                        </DropdownMenuItem> */}
+
+                        <AlertDialogTrigger asChild>
+                            <DropdownMenuItem
+                                disabled={isPending}
+                                onSelect={(e) => {
+                                    e.preventDefault();
+                                    setIsDialogOpen(true);
+                                }}
+                                className="cursor-pointer text-red-600 focus:text-red-600"
+                            >
+                                {isPending ? 'Deleting...' : 'Delete'}
+                            </DropdownMenuItem>
+                        </AlertDialogTrigger>
+                    </DropdownMenuGroup>
+                </DropdownMenuContent>
+            </DropdownMenu>
+
+            <AlertDialogContent>
+                <AlertDialogHeader className="items-center">
+                    <div className="bg-destructive/10 mx-auto mb-2 flex size-12 items-center justify-center rounded-full">
+                        <TriangleAlertIcon className="text-destructive size-6" />
+                    </div>
+                    <AlertDialogTitle>
+                        Are you absolutely sure you want to delete?
+                    </AlertDialogTitle>
+                    <AlertDialogDescription className="text-center">
+                        This action cannot be undone. This will permanently delete this
+                        form along with all its data!
+                    </AlertDialogDescription>
+                </AlertDialogHeader>
+                <AlertDialogFooter>
+                    <AlertDialogCancel disabled={isPending}>Cancel</AlertDialogCancel>
+                    <AlertDialogAction
+                        onClick={(e) => {
+                            e.preventDefault();
+                            handleDelete();
+                        }}
                         disabled={isPending}
-                        onSelect={handleDelete}
-                        className="cursor-pointer text-red-600 focus:text-red-600"
+                        className="bg-destructive dark:bg-destructive/60 hover:bg-destructive focus-visible:ring-destructive text-white"
                     >
-                        {isPending ? 'Deleting...' : 'Delete'}
-                    </DropdownMenuItem>
-                </DropdownMenuGroup>
-            </DropdownMenuContent>
-        </DropdownMenu>
+                        {isPending ? 'Deleting...' : 'Yes, Delete Form'}
+                    </AlertDialogAction>
+                </AlertDialogFooter>
+            </AlertDialogContent>
+        </AlertDialog>
     );
 }
